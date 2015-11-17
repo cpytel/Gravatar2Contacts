04ed6de7415cea43b291fb8a15fc29bf69a7bf61
Stefan Paychère <stefan.paychere@epyx.ch>
2015-11-17 11:48:59 +0100

    Fixed build for Xcode 7.1.1
    Signed-off-by: Stefan Paychère <stefan.paychere@epyx.ch>

--- Contacts/main.swift
+++ Contacts/main.swift
@@ -14,10 +14,10 @@ let people = addressbook.people() as! [ABPerson]
 
 for person in people {
     if let emails = person.emails() {
-        println("Finding Gravatar for \(person.firstName()) \(person.lastName())...")
+        print("Finding Gravatar for \(person.firstName()) \(person.lastName())...")
         
         for i in 0..<emails.count() {
-            let email = Email(address: emails.valueAtIndex(i) as String)
+            let email = Email(address: emails.valueAtIndex(i) as! String)
             
             person.setImageDataFromURL(email.gravatarURL())
         }
